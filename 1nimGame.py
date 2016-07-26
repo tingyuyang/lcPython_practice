69d13cdb9dbba78f9f3837a906381abb3a2cd0cf
Tingyu Yang <tingyuyang@lclark.edu>
2016-07-25 18:26:22 -0700

    update correct solution

--- 1nimGame.py
+++ 1nimGame.py
@@ -9,15 +9,20 @@ Write a function to determine whether you can win the game given the number of s
 For example, if there are 4 stones in the heap, 
 then you will never win the game: no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
 """
+# i forgot the case when the stone # is < 4! need to be more careful
 class Solution(object):
     def canWinNim(self, n):
         """
         :type n: int
         :rtype: bool
         """
+        # ture is i win the game
         left = n-1
         var = False
-        if (n % 4 == 0):
+        if (n < 4):# if the stone is 1,2,3, i win
             var = True
+        else:
+            if (n % 4 != 0): # if n != 4*x, then i win 
+                var = True
         return var
  # my solution is partially guessing. if the stone number is 4, 8, 12.... i will not win for sure.
