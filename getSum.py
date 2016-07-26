727e571bd8b649f5f89b65e3f271ff8ce18f8ea5
Tingyu Yang <tingyuyang@lclark.edu>
2016-07-25 19:20:32 -0700

    Create getSum.py

---
+++ getSum.py
@@ -0,0 +1,27 @@
+#clearly, the solution is not efficient enough.
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        count = 0
+        if (a>0):
+            while (a>0):
+                count = count +1
+                a = a-1
+        if (a<0):
+         	while (a !=0):
+         		count = count -1
+         		a = a+1
+        if (b>0):
+            while (b>0):
+                count = count +1
+                b = b -1
+        if (b<0):
+         	while (b !=0):
+         		count = count -1
+         		b = b+1 		
+        return (count)  
