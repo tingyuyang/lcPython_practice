9377a1e3658f1840b5c267c5d5d239c6c59f1440
Tingyu Yang <tingyuyang@lclark.edu>
2016-07-27 02:03:40 -0700

    Create subString.py

---
+++ subString.py
@@ -0,0 +1,8 @@
+# java code https://github.com/tingyuyang/Java_Practice/blob/master/subString.java
+
+def subString(n):
+	s = str(n)
+	l = len(s) # length for the int
+	checkChar = s[0]
+	result = s[0:1] +s[2:]
+	print (result)
