5710264f02fe122e34cd70617fc18b3cb7102ab2
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-04 15:08:24 -0700

    Update 2remove.py

--- 2remove.py
+++ 2remove.py
@@ -2,9 +2,6 @@
 #123-> 23,13
 
 def remove(n):
-	if (n<10 or n>1000000000):
-		print('The input number is out of range, please enter another number')
-		return -1
 	s = str(n) # change the int into string
 	min = n
 	check = s[0]
