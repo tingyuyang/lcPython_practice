92f54a990e753e09e08b7db7ff0483715f3e390e
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-07 01:14:53 -0700

    Create 1maximun_depth_binaryTree.py

---
+++ 1maximun_depth_binaryTree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if (root == None): # similar as "if root is None".
+            return 0
+        leftMax = self.maxDepth(root.left)
+        rightMax = self.maxDepth(root.right)
+        return max(leftMax,rightMax)+1     #"+1" is for root
