3e1b265f0feea068fe1ea63af6dd4f6bac8174be
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-12 19:35:21 -0700

    Update 1sum.py

--- 1sum.py
+++ 1sum.py
@@ -1,4 +1,6 @@
 #Problem: You are given an integer n. Count result of sum=  1+2+ ... + n
+
+#the SlOWEST SOLUTION
 """
 def slow_solution(n):
   result = 0
@@ -10,10 +12,18 @@ def slow_solution(n):
 because this method is nested loop, so the time complexity wil be O(N^2), 
 so here is the fast one
 """
-n=5
-result = 0
-for i in range(n):
-	result = result + (i + 1) # i will be 0,1,2,3 every time the for loop runs
-print(result)
-#print out 15
+
+# FASTER SOLUTION
+def faster_solution(n):
+	result = 0
+	for i in range(n):
+		result = result + (i + 1) # i will be 0,1,2,3 every time the for loop runs
+	print(result)
+# if n = 5, result is 15
 # very interesting, the complexity will be O(N) then ::)
+
+# FASTEST SOLUTION with complexity of O(1)
+def model_solution(n):
+	result = n * (n + 1) // 2 #based on our middle school math knowledge.FUNCTION
+	return result
+
