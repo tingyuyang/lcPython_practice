a413a244027a036227e3d09bb8019e286b2d0e4b
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-13 19:32:53 -0700

    Update 1smallest.py

--- 1smallest.py
+++ 1smallest.py
@@ -1,20 +1,20 @@
 def kthsmallest(arr1, arr2, k):
     if len(arr1) == 0:
-        return arr2[k]
+        return arr2[k] # coz it is sorted array, so return kth index
     elif len(arr2) == 0:
         return arr1[k]
-    mida1 = int(len(arr1)/2)
+    mida1 = int(len(arr1)/2) #the middle index of arr1
     mida2 = int(len(arr2)/2)
-    if mida1+mida2<k:
-        if arr1[mida1]>arr2[mida2]:
-            return kthlargest(arr1, arr2[mida2+1:], k-mida2-1)
+    if mida1+mida2<k: # if the sum of middle index of both array less than k
+        if arr1[mida1]>arr2[mida2]: # if 
+            return kthsmallest(arr1, arr2[mida2+1:], k-mida2-1)
         else:
-            return kthlargest(arr1[mida1+1:], arr2, k-mida1-1)
+            return kthsmallest(arr1[mida1+1:], arr2, k-mida1-1)
     else:
         if arr1[mida1]>arr2[mida2]:
-            return kthlargest(arr1[:mida1], arr2, k)
+            return kthsmallest(arr1[:mida1], arr2, k)
         else:
-            return kthlargest(arr1, arr2[:mida2], k)
+            return kthsmallest(arr1, arr2[:mida2], k)
   """      
 a = (0,2,9,10,12,13,14,16)
 b = (8,6) 
