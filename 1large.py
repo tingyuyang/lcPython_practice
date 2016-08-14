4e3c854d2eb792663bb1008d5f33d40304ba4667
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-14 03:20:15 -0700

    Create 1large.py

---
+++ 1large.py
@@ -0,0 +1,22 @@
+#need further revise
+def kthlargest(arr1, arr2, k):
+    if len(arr1) == 0:
+        return arr2[k] # coz it is sorted array, so return kth index
+    elif len(arr2) == 0:
+        return arr1[k]
+    print("!!!!!!!!!!")
+    mida1 = int(len(arr1)/2) #the middle index of arr1
+    mida2 = int(len(arr2)/2)
+    if mida1+mida2<k: # if the sum of middle index of both array less than k
+        if arr1[mida1]>arr2[mida2]:
+        	print("k is")
+        	print(k)
+        	return kthlargest(arr1[:mida1], arr2, k-mida1-1)
+        else:
+        	print("SHUT")
+        	return kthlargest(arr1, arr2[:mida2], k-mida2)
+    else:
+        if arr1[mida1]>arr2[mida2]:
+        	return kthlargest(arr1, arr2[mida2+1:], k+mida1) #this line is fixed
+        else:
+        	return kthlargest(arr1[mida1+1:], arr2, k+mida2) #fixed
