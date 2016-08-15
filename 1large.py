817812f3a13cc84e3f0cf52d746d543bc8ad5510
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-14 21:53:26 -0700

    Update 1large.py

--- 1large.py
+++ 1large.py
@@ -1,20 +1,17 @@
 #need further revise
+#https://www.glassdoor.com/Interview/Get-the-kth-largest-number-from-two-sorted-arrays-QTN_290643.htm
 def kthlargest(arr1, arr2, k):
     if len(arr1) == 0:
         return arr2[k] # coz it is sorted array, so return kth index
     elif len(arr2) == 0:
         return arr1[k]
-    print("!!!!!!!!!!")
     mida1 = int(len(arr1)/2) #the middle index of arr1
     mida2 = int(len(arr2)/2)
     if mida1+mida2<k: # if the sum of middle index of both array less than k
         if arr1[mida1]>arr2[mida2]:
-        	print("k is")
-        	print(k)
-        	return kthlargest(arr1[:mida1], arr2, k-mida1-1)
+        	return kthlargest(arr1[:mida1+1], arr2[mida2:], k-mida1-1)
         else:
-        	print("SHUT")
-        	return kthlargest(arr1, arr2[:mida2], k-mida2)
+        	return kthlargest(arr1[mida1:], arr2[:mida2+1], k-mida2)
     else:
         if arr1[mida1]>arr2[mida2]:
         	return kthlargest(arr1, arr2[mida2+1:], k+mida1) #this line is fixed
