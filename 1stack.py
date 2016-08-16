3df9f73b87578df7197212f967091edbe3c7ad58
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-16 16:26:46 -0700

    Create 1stack.py

---
+++ 1stack.py
@@ -0,0 +1,42 @@
+# just a simple stack practice
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+s=Stack()
+
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
+
+"""
+True
+dog
+3
+False
+8.4
+True
+2
+"""
