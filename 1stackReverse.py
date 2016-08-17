4bd6ae7cf757acfd89ca19dc6abf0300315e6343
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-16 17:02:34 -0700

    Update and rename 1stackReverse to 1stackReverse.py

--- 1stackReverse.py
+++ 1stackReverse.py
@@ -1,4 +1,5 @@
 # Write a function revstring(mystr) that uses a stack to reverse the characters in a string.
+# Header check: https://github.com/tingyuyang/lcPython_practice/blob/master/1stack.py
 
 def revstring(mystr):
     # your code here
