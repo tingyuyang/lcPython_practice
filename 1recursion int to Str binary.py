8ec900cd479f2b6291594f86befa40eaf5ce44a4
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-18 22:29:55 -0700

    Update 1recursion int to Str binary.py

--- 1recursion int to Str binary.py
+++ 1recursion int to Str binary.py
@@ -6,5 +6,8 @@ def convert(n,base):
 		return convert(n//base,base) + convertBase[n-base*(n//base)] #method to found reminder, could also be "convertBase[n%base]"
 print(convert(1453,16))
 
-#>> 5AD
+# >> 5AD
 # so the first calculation will be at the end of the string.
+# 1453/16==90......13
+# 13 the reminder will be 'D' (10 will be 'A')
+# 'D' will be the end of output string
