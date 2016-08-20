267fca3ccc580d263a8d480cc7531937e162baf9
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-20 02:51:32 -0700

    Update 1sort Selection Sort.py

--- 1sort Selection Sort.py
+++ 1sort Selection Sort.py
@@ -1,26 +1,13 @@
 def selectionSort(list): #arrange the list in order
-	length=len(list)-1
-	currentMax=list[length]
-	for i in range(0,len(list)):
-		if list[i]>currentMax:
-			currentMax=list[i]
-			temp = list[length]
-			list[length]=currentMax
-			list[i] = temp
-			length=length-1
-
-list=[3,6,7,9,8]
+	for i in range(len(list)-1,0,-1):
+		maxPosition = 0
+		for current in range(1,i+1):
+			if list[current]>list[maxPosition]:
+				maxPosition=current
+		temp = list[i]
+		list[i]=list[maxPosition]
+		list[maxPosition]=temp
+			
+list=[54,26,93,17,77,31,44,55,20]
 selectionSort(list)
 print(list)
-"""
-def selectionSort(alist):
-   for fillslot in range(len(alist)-1,0,-1):
-       positionOfMax=0
-       for location in range(1,fillslot+1):
-           if alist[location]>alist[positionOfMax]:
-               positionOfMax = location
-
-       temp = alist[fillslot]
-       alist[fillslot] = alist[positionOfMax]
-       alist[positionOfMax] = temp
-"""
