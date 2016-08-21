a9fbd8d849ee7991e5b49d61997ae19e3e9951a9
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-21 00:17:10 -0700

    Update 1sort insertion Sort.py

--- 1sort insertion Sort.py
+++ 1sort insertion Sort.py
@@ -5,7 +5,7 @@ def insertionSort(list):
 		while index>0 and storedValue< list[index-1]: #if storedValue smaller than index-1, we will stop the loop
 			list[index]=list[index-1]
 			index=index-1
-		list[index]=storedValue #must be "index" not "i"
+		list[index]=storedValue #must be "index" not "i",because it will be an empty slot once the while loop stopped, we will put the stored value in this empty slot
 		
 list=[54,26,93,17,77,31,44,55,20]
 insertionSort(list)
