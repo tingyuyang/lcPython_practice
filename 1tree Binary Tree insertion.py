a955569382e0bbbc47df24a7303221a6b9464361
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-22 19:46:47 -0700

    Create 1tree Binary Tree insertion.py

---
+++ 1tree Binary Tree insertion.py
@@ -0,0 +1,26 @@
+def BinaryTree(r):
+    return [r, [], []]
+
+def insertLeft(root,newBranch):
+    print(root)
+    t = root.pop(1)
+    print (t)
+    print("///////")
+    if len(t) > 1:
+        root.insert(1,[newBranch,t,[]])
+    else:
+        root.insert(1,[newBranch, [], []])
+    return root
+
+r = BinaryTree(3)
+insertLeft(r,4)
+insertLeft(r,5)
+
+"""
+[3, [], []]
+[]
+///////
+[3, [4, [], []], []]
+[4, [], []]
+///////
+"""
