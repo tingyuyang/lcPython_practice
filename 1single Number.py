60077002bd7830cbd97c2fc0a89b86f54dbee936
Tingyu Yang <tingyuyang@lclark.edu>
2016-09-12 01:14:24 -0700

    Update 1single Number.py

--- 1single Number.py
+++ 1single Number.py
@@ -7,12 +7,16 @@ Your algorithm should have a linear runtime complexity. > Could you implement it
 
 #simple solution:
 num=[1,1,2,2,3]
-def singleNumber(num):
-	num.sort()
-	count = 0
-	for i in range(1,len(num)-1):
-		if num[i]!=num[i-1] and num[i]!=num[i+1]:
-			return (num[i])
-	return(num[len(num)-1])
+def singleNumber(self,nums):
+    	if len(nums)==1:
+    		return nums[len(nums)-1]
+    	nums.sort()
+    	for i in range(1,len(nums)-1):
+    		if nums[i]!=nums[i-1] and nums[i]!=nums[i+1]:
+    			return (nums[i])
+    	if nums[0]!=nums[1]:
+    		return nums[0]
+    	else:
+    		return(nums[len(nums)-1])
 
 print(singleNumber(num))
